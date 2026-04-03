d1270e5ca97744591134817a32ebdbcc37279dba
thelamesaucegit <100131464+thelamesaucegit@users.noreply.github.com>
2026-04-03 14:59:38 -0500

    Add ArgentumData class with game state models
    for generating json game states using new format

---
+++ forge-game/src/main/java/forge/argentum/data/ArgentumData.java
@@ -0,0 +1,65 @@
+// src/main/java/forge/argentum/data/ArgentumData.java
+package forge.argentum.data;
+
+import java.util.List;
+import java.util.Map;
+
+// Using static nested classes keeps all our data models in one file.
+public class ArgentumData {
+
+    public static class SpectatorStateUpdate {
+        public String gameSessionId;
+        public ClientGameState gameState;
+        public String player1Id;
+        public String player2Id;
+        public String player1Name;
+        public String player2Name;
+        public String currentPhase;
+        public String activePlayerId;
+        public String priorityPlayerId;
+        public boolean isReplay = true;
+        public Object combat = null; // To be defined later
+    }
+
+    public static class ClientGameState {
+        public Map<String, ClientCard> cards;
+        public List<ClientZone> zones;
+        public List<ClientPlayer> players;
+        public String currentPhase;
+        public String currentStep;
+        public String activePlayerId;
+        public String priorityPlayerId;
+        public int turnNumber;
+        public boolean isGameOver;
+        public String winnerId;
+        public Object combat = null; // To be defined later
+        public List<String> gameLog;
+    }
+
+    public static class ClientCard {
+        public String entityId;
+        public String name;
+        public String imageUri;
+        public List<String> cardTypes;
+        public boolean isTapped;
+        public boolean isAttacking = false; // We will populate this during combat phase
+        public boolean isBlocking = false; // We will populate this during combat phase
+        public Integer power;
+        public Integer toughness;
+        public int damage;
+        public List<Object> targets; // To be defined later
+    }
+
+    public static class ClientZone {
+        public String zoneId;
+        public String type;
+        public String ownerId;
+        public List<String> cardIds;
+    }
+
+    public static class ClientPlayer {
+        public String playerId;
+        public String name;
+        public int life;
+    }
+}
