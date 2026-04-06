811eefaebcc4c1b736b3e4dccc05680d48daed28
thelamesaucegit <100131464+thelamesaucegit@users.noreply.github.com>
2026-04-05 21:41:41 -0500

    Update ArgentumStateLogger.java

--- forge-game/src/main/java/forge/argentum/ArgentumStateLogger.java
+++ forge-game/src/main/java/forge/argentum/ArgentumStateLogger.java
@@ -19,7 +19,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
