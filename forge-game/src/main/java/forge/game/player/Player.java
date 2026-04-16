667432a15f765ac0932e716ff09b07294693ed29
thelamesaucegit <100131464+thelamesaucegit@users.noreply.github.com>
2026-04-16 16:13:54 -0500

    Update Player.java

--- forge-game/src/main/java/forge/game/player/Player.java
+++ forge-game/src/main/java/forge/game/player/Player.java
@@ -1267,7 +1267,7 @@ public class Player extends GameEntity implements Comparable<Player> {
         if (!drawn.isEmpty()) {
         // The phase handler will tell us if we are in the draw step or not.
         String stepName = game.getPhaseHandler().is(PhaseType.DRAW) ? "DRAW_STEP" : "EFFECT_DRAW";
-        ArgentumStateLogger.logState(game, stepName);
+      //  ArgentumStateLogger.logState(game, stepName);
     }
         return drawn;
     }
